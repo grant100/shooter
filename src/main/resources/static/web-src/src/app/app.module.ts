eb5bddeb47500096f0c0a18405f92f733f26ca0f
Grant Sowards <gsowards100@gmail.com>
2018-05-05 16:12:54 -0600

    Angular 6 - Angular Material

--- src/main/resources/static/web-src/src/app/app.module.ts
+++ src/main/resources/static/web-src/src/app/app.module.ts
@@ -6,14 +6,95 @@ import { AppComponent } from './app.component';
 import {LobbyComponent} from "./lobby/lobby.component";
 import {GameService} from "./services/GameService";
 import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CdkTableModule} from '@angular/cdk/table';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+} from '@angular/material';
 
-
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+  ]
+})
+export class MaterialModule {}
 @NgModule({
   declarations: [
     AppComponent,LobbyComponent
   ],
   imports: [
-    BrowserModule,HttpClientModule
+    BrowserModule,HttpClientModule, BrowserAnimationsModule, MaterialModule, ReactiveFormsModule,FormsModule
   ],
   providers: [GameService],
   bootstrap: [AppComponent]
--- src/main/resources/static/web-src/src/app/services/GameService.ts
+++ src/main/resources/static/web-src/src/app/services/GameService.ts
@@ -3,21 +3,22 @@ import {Game} from "../models/Game";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable()
-export class GameService{
+export class GameService {
 
-  constructor(private http: HttpClient){}
-  getGames(): Game[]{
-    let games: Game[]=[];
+  constructor(private http: HttpClient) {
+  }
+
+  getGames(): Game[] {
+    let games: Game[] = [];
     console.log('firing /games GET....');
-    var result = this.http.get("/games").subscribe(
-      data => new Game(data['isEmpty'],data['size'],data['gameId'])
-    );
-    console.log(result);
-    games.push(new Game(false,1,1));
-    games.push(new Game(false,1,2));
-    games.push(new Game(false,1,3));
-    games.push(new Game(false,1,4));
-   // games.push(new Game(false,1,5));
+    var result = this.http.get<Game[]>("/games").subscribe(value => games = value);
+
+    console.log(games);
+    //games.push(new Game(false,1,1));
+    //games.push(new Game(false,1,2));
+    //games.push(new Game(false,1,3));
+    //games.push(new Game(false,1,4));
+    // games.push(new Game(false,1,5));
     return games;
   }
 }
